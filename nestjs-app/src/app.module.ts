7eb2998f6ec5c292385370b7186711c2caecc920
Daniel Montilla <danielmontillanavas@gmail.com>
2024-10-24 21:58:43 +0400

    feat(): Connect to DB with DB module

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -1,30 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { readFileSync } from 'fs';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // Load .env file and make it globally available
-    PostsModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-        ssl: {
-            rejectUnauthorized: true,
-            ca: readFileSync(configService.get<string>('DB_SSL_CA')).toString(),
-        }
+      PostsModule,
+      ConfigModule.forRoot({
+        validationSchema: Joi.object({
+            DB_HOST: Joi.string().required(),
+            DB_PORT: Joi.number().required(),
+            DB_USERNAME: Joi.string().required(),
+            DB_PASSWORD: Joi.string().required(),
+            DB_NAME: Joi.string().required(),
+            DB_SSL_CA: Joi.string().required(),
+            PORT: Joi.number()
+        })
       }),
-    }),
+      DatabaseModule
   ],
   controllers: [],
   providers: [],
