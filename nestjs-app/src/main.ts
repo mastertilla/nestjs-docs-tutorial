41210737ce0cd97b74f4441f89a8aa17ad2756a8
Daniel Montilla <danielmontillanavas@gmail.com>
2024-10-29 08:15:11 +0400

    feat(): Custom exception + base validation request data

--- nestjs-app/src/main.ts
+++ nestjs-app/src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   app.use(cookieParser())
   const config = new DocumentBuilder()
     .setTitle('Posts Module')
